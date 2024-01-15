1b57e332150b4a8cf8e286cd89b29c9af9f58de8
PariaRF <PariaRF>
2024-01-15 13:04:56 +0330

    confirm delete with useRemoveProjects

--- src/features/projects/ProjectRow.jsx
+++ src/features/projects/ProjectRow.jsx
@@ -6,9 +6,13 @@ import { toPersianNumberWithComma } from "../../utils/toPersianNumbers";
 import trancateText from "../../utils/truncateText";
 import { HiOutlineTrash } from "react-icons/hi";
 import { TbPencilMinus } from "react-icons/tb";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import useRemoveProject from "./useRemoveProject";
 
 function ProjectRow({ project, index }) {
   const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+  const { removeProject, isDeleting } = useRemoveProject();
 
   return (
     <Table.Row>
@@ -41,14 +45,30 @@ function ProjectRow({ project, index }) {
           </button>
           <Modal
             open={isEditOpen}
-            title="modal Title"
+            title={`ویرایش ${project.title}`}
             onClose={() => setIsEditOpen(false)}
           >
-            this is modal
+            this is Edit modal
           </Modal>
-          <button>
+          <button onClick={() => setIsDeleteOpen(true)}>
             <HiOutlineTrash className="w-5 h-5 text-error" />
           </button>
+          <Modal
+            open={isDeleteOpen}
+            title={`حذف ${project.title}`}
+            onClose={() => setIsDeleteOpen(false)}
+          >
+            <ConfirmDelete
+              resourceName={project.title}
+              onClose={() => setIsDeleteOpen(false)}
+              onConfirm={() =>
+                removeProject(project._id, {
+                  onSuccess: () => setIsDeleteOpen(false),
+                })
+              }
+              disabled={false}
+            />
+          </Modal>
         </div>
       </td>
     </Table.Row>

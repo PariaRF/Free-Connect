5543c9bb8069cb3892c5a598036b439ba1c3772f
PariaRF <PariaRF>
2024-02-20 18:21:12 +0330

    added table of projects and proposals

--- src/features/admin/AdminLayout.jsx
+++ src/features/admin/AdminLayout.jsx
@@ -24,7 +24,7 @@ function AdminLayout() {
           <HiCollection />
           <span>پروژه ها</span>
         </CustomNavLink>
-        <CustomNavLink to="projects">
+        <CustomNavLink to="proposals">
           <HiOutlineViewGrid />
           <span>درخواست ها</span>
         </CustomNavLink>
